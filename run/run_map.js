cdcb45b8a7d8d4c99135fdfc3b349ef1df399278
Phil Cummins <philhoughtoncummins@gmail.com>
2022-04-05 07:58:24 +0100

    adds run folder

---
+++ run/run_map.js
@@ -0,0 +1,28 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import GPX from 'ol/format/GPX';
+import OSM from 'ol/source/OSM';
+import VectorSource from 'ol/source/Vector';
+import View from 'ol/View';
+import {Vector as VectorLayer} from 'ol/layer';
+import TileLayer from 'ol/layer/Tile';
+import {fromLonLat} from 'ol/proj'
+
+const map = new Map({
+    target: 'map',
+    layers: [
+        new TileLayer({
+        source: new OSM(),
+    }), 
+    new VectorLayer({
+        source: new VectorSource({
+          url: 'cleaned_threlkeld_old_railway.gpx',
+          format: new GPX(),
+        }),
+    })
+    ],
+    view: new View({
+      center: fromLonLat([-3.1342, 54.6038]),
+      zoom: 13
+    })
+  });
