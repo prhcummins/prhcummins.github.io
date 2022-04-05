f6db6efbd749a0518d04817acc41e63921bff3b9
Phil Cummins <philhoughtoncummins@gmail.com>
2022-04-05 09:06:41 +0100

    prepares for github-pages

---
+++ run_map.js
@@ -0,0 +1,31 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import GPX from 'ol/format/GPX';
+import OSM from 'ol/source/OSM';
+import Stamen from 'ol/source/Stamen';
+import VectorSource from 'ol/source/Vector';
+import View from 'ol/View';
+import {Vector as VectorLayer} from 'ol/layer';
+import TileLayer from 'ol/layer/Tile';
+import {fromLonLat} from 'ol/proj'
+
+const map = new Map({
+    target: 'map',
+    layers: [
+    new TileLayer({
+      source: new Stamen({
+        layer: 'terrain-background',
+      }),
+    }),
+    new VectorLayer({
+        source: new VectorSource({
+          url: 'cleaned_threlkeld_old_railway.gpx',
+          format: new GPX(),
+        }),
+    })
+    ],
+    view: new View({
+      center: fromLonLat([-3.0068, 54.4792]),
+      zoom: 11
+    })
+  });
