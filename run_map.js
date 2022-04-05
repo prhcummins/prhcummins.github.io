92e4a6278a6266de184251029c9ff3057a25dd65
Phil Cummins <philhoughtoncummins@gmail.com>
2022-04-05 10:48:53 +0100

    adds blaeberry and la trig

--- run_map.js
+++ run_map.js
@@ -1,13 +1,28 @@
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import GPX from 'ol/format/GPX';
-import OSM from 'ol/source/OSM';
 import Stamen from 'ol/source/Stamen';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
 import {Vector as VectorLayer} from 'ol/layer';
 import TileLayer from 'ol/layer/Tile';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
 import {fromLonLat} from 'ol/proj'
+
+const style = {
+  'LineString': new Style({
+    stroke: new Stroke({
+      color: '#FAA500',
+      width: 2,
+    }),
+  }),
+  'MultiLineString': new Style({
+    stroke: new Stroke({
+      color: '#FAA500',
+      width: 2,
+    }),
+  }),
+};
 
 const map = new Map({
     target: 'map',
@@ -18,16 +33,40 @@ const map = new Map({
       }),
     }),
     new VectorLayer({
-        source: new VectorSource({
-          url: 'cleaned_threlkeld_old_railway.gpx',
-          format: new GPX(),
-        }),
+      source: new VectorSource({
+        url: 'cleaned_threlkeld_old_railway.gpx',
+        format: new GPX(),
+      }),
+      style: function (feature) {
+        return style[feature.getGeometry().getType()];
+      },
     }),
     new VectorLayer({
       source: new VectorSource({
         url: 'cleaned_derwentwater_loop.gpx',
         format: new GPX(),
       }),
+      style: function (feature) {
+        return style[feature.getGeometry().getType()];
+      },
+    }),
+    new VectorLayer({
+      source: new VectorSource({
+        url: 'cleaned_blaeberry.gpx',
+        format: new GPX(),
+      }),
+      style: function (feature) {
+        return style[feature.getGeometry().getType()];
+      },
+    }),
+    new VectorLayer({
+      source: new VectorSource({
+        url: 'cleaned_la_trig.gpx',
+        format: new GPX(),
+      }),
+      style: function (feature) {
+        return style[feature.getGeometry().getType()];
+      },
   })
     ],
     view: new View({
